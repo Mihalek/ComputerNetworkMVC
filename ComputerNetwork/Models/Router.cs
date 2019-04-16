817b33d01194fcd28484ac5b41a886fa747e766d
Michal Marciniak <michalmarekmarciniak@gmail.com>
2019-04-16 19:30:00 +0200

    16.04.2019

---
+++ ComputerNetwork/Models/Router.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComputerNetwork.Models
+{
+    public class Router
+    {
+    }
+}
--- ComputerNetwork/Startup.cs
+++ ComputerNetwork/Startup.cs
@@ -15,6 +15,7 @@ namespace ComputerNetwork
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,13 @@ namespace ComputerNetwork
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            /*  app.Run(async (context) =>
+              {
+                  await context.Response.WriteAsync("Hello World!");
+              });*/
+
+            app.UseStaticFiles(); //Musi istniec aby uzywac plikow statycznych
+            app.UseMvcWithDefaultRoute(); // Musi istniec aby zaladowac nasz widok
         }
     }
 }
