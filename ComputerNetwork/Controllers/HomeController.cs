f46f839072fb437668bf9fc43181772520daffc9
Michal Marciniak <michalmarekmarciniak@gmail.com>
2019-05-09 01:34:53 +0200

    09.05.2019

--- ComputerNetwork/Controllers/HomeController.cs
+++ ComputerNetwork/Controllers/HomeController.cs
@@ -42,11 +42,31 @@ namespace ComputerNetwork.Controllers
             */
 
         [HttpPost]
-        public async Task<IActionResult> Index(Computer router)
+        public async Task<IActionResult> Index(Router router, Computer computer, Edge edge, Switch @switch)
         {
-            await _context.Computers.AddAsync(router);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
+            string item = HttpContext.Request.Form["nameofelement"].ToString();
+            switch (item)
+            {
+                case "Router":
+                    await _context.Routers.AddAsync(router);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                case "Switch":
+                    await _context.Switches.AddAsync(@switch);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                case "Computer":
+                    await _context.Computers.AddAsync(computer);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                case "Edge":
+                    await _context.Edges.AddAsync(edge);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+            }
+
+            return View();
+
 
         }
 
