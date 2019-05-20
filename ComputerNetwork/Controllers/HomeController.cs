aced70dcaf4230a7e598153fc044bd00b492337d
Michal Marciniak <michalmarekmarciniak@gmail.com>
2019-05-20 22:17:27 +0200

    20.05.2019

--- ComputerNetwork/Controllers/HomeController.cs
+++ ComputerNetwork/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ComputerNetwork.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ComputerNetwork.Controllers
@@ -19,32 +20,173 @@ namespace ComputerNetwork.Controllers
         public HomeController(ApplicationDbContext context) {
             _context = context;
 
+
         }
+
+        [HttpPost]
+        public IActionResult Clean()
+        {
+            _context.Routers.RemoveRange(_context.Routers);
+            _context.Switches.RemoveRange(_context.Switches);
+            _context.Computers.RemoveRange(_context.Computers);
+            _context.Edges.RemoveRange(_context.Edges);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete()
+        {
+            string id = HttpContext.Request.Form["idofelement"].ToString();
+            string item = HttpContext.Request.Form["nameofelement"].ToString();
+            switch (item)
+            {
+
+                case "Router":
+                    try
+                    {
+                        _context.Routers.Remove(_context.Routers
+                                .SingleOrDefault(r => r.IdOfElement == id));
+                        _context.SaveChanges();
+                        return RedirectToAction("index");
+                    }
+                    catch (Exception ex)
+                    {
+                        return RedirectToAction("index");
+                    }
+
+                case "Switch":
+                    try
+                    {
+                        _context.Switches.Remove(_context.Switches
+                                .SingleOrDefault(r => r.IdOfElement == id));
+                        _context.SaveChanges();
+                        return RedirectToAction("index");
+                    }
+                    catch (Exception ex)
+                    {
+                        return RedirectToAction("index");
+                    }
+
+                case "Computer":
+
+                    try
+                    {
+                        _context.Computers.Remove(_context.Computers
+                                .SingleOrDefault(r => r.IdOfElement == id));
+                        _context.SaveChanges();
+                        return RedirectToAction("index");
+                    }
+                    catch (Exception ex)
+                    {
+                        return RedirectToAction("index");
+                    }
+                case "Edge":
+                    try
+                    {
+                        _context.Edges.Remove(_context.Edges
+                                .SingleOrDefault(r => r.IdOfElement == id));
+                        _context.SaveChanges();
+                        return RedirectToAction("index");
+                    }
+                    catch (Exception ex)
+                    {
+                        return RedirectToAction("Index");
+                    }
+
+            }
+            return View("index");
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> Index(Router router, Computer computer, Edge edge, Switch @switch)
         {
 
             if (ModelState.IsValid)
             {
+                bool contactExists;
                 string item = HttpContext.Request.Form["nameofelement"].ToString();
+                string id = HttpContext.Request.Form["idofelement"].ToString();
                 switch (item)
                 {
                     case "Router":
-                        await _context.Routers.AddAsync(router);
-                        await _context.SaveChangesAsync();
-                        return RedirectToAction("Index");
+                        contactExists = _context.Routers.Any(r => r.IdOfElement.Equals(id));
+                        if (!contactExists)
+                        {
+                            await _context.Routers.AddAsync(router);
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            var oldrouter = await this._context.Routers
+                            .SingleOrDefaultAsync(r => r.IdOfElement == id);
+                            oldrouter.IpAddress = router.IpAddress;
+                            oldrouter.Mask = router.Mask;
+                            oldrouter.Gateway = router.Gateway;
+                        
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction("Index");
+                        }
                     case "Switch":
-                        await _context.Switches.AddAsync(@switch);
-                        await _context.SaveChangesAsync();
-                        return RedirectToAction("Index");
+                        contactExists = _context.Switches.Any(s => s.IdOfElement.Equals(id));
+                        if (!contactExists)
+                        {
+                            await _context.Switches.AddAsync(@switch);
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            var oldswitch = await this._context.Switches
+                            .SingleOrDefaultAsync(r => r.IdOfElement == id);
+                            oldswitch.IpAddress = @switch.IpAddress;
+                            oldswitch.Mask = @switch.Mask;
+                            oldswitch.Gateway = @switch.Gateway;
+
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction("Index");
+                        }
                     case "Computer":
-                        await _context.Computers.AddAsync(computer);
-                        await _context.SaveChangesAsync();
-                        return RedirectToAction("Index");
+                        contactExists = _context.Computers.Any(c => c.IdOfElement.Equals(id));
+                        if (!contactExists)
+                        {
+                            await _context.Computers.AddAsync(computer);
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            var oldcomputer = await this._context.Computers
+                            .SingleOrDefaultAsync(c => c.IdOfElement == id);
+                            oldcomputer.IpAddress = computer.IpAddress;
+                            oldcomputer.Mask = computer.Mask;
+                            oldcomputer.Gateway = computer.Gateway;
+
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction("Index");
+                        }
                     case "Edge":
-                        await _context.Edges.AddAsync(edge);
-                        await _context.SaveChangesAsync();
-                        return RedirectToAction("Index");
+                        contactExists = _context.Edges.Any(e => e.IdOfElement.Equals(id));
+                        if (!contactExists)
+                        {
+                            await _context.Edges.AddAsync(edge);
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            var oldedge = await this._context.Edges
+                            .SingleOrDefaultAsync(e => e.IdOfElement == id);
+                            oldedge.IpAddress = edge.IpAddress;
+                            oldedge.Mask = edge.Mask;
+                            oldedge.Gateway = edge.Gateway;
+
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction("Index");
+                        }
                 }
             }
             return View();
