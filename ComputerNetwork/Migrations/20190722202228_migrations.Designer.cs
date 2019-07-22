1ee504ca956223d47db2a4534d1435f5dac0c368
Michal Marciniak <michalmarekmarciniak@gmail.com>
2019-07-22 22:42:01 +0200

    22.07.2019

--- ComputerNetwork/Migrations/20190722202228_migrations.Designer.cs
+++ ComputerNetwork/Migrations/20190722202228_migrations.Designer.cs
@@ -9,14 +9,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ComputerNetwork.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20190717122835_migrations")]
+    [Migration("20190722202228_migrations")]
     partial class migrations
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.8-servicing-32085")
+                .HasAnnotation("ProductVersion", "2.1.11-servicing-32099")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
